b05cc79d4c1506bd92ab08a4d677ad295b5002e8
chry-u <chrysanthemum1988@gmail.com>
2018-10-31 19:48:54 +0800

    test use rsa login server

--- devices/views.py
+++ devices/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 import winrm
+#winrm 0.3.0
+import fabric
+#fabric 2.4.0
 # Create your views here.
 s = winrm.Session('192.168.209.162', auth=('*', '*'),transport='ntlm')
 # s = winrm.Session('https://172.30.200.149:5986/wsman', auth= ('chry', 'p'))
@@ -21,8 +24,31 @@ def auto_ss():
     p.cleanup_command(shell_id, command_id)
     p.close_shell(shell_id)
 #rest_cp = s.run_ps('Restart-Computer -Force')
-rest_pro= s.run_ps('Get-Process | Where-Object {$_.ProcessName.Contains("cloudia") } | Stop-Process -Force')
+#rest_pro= s.run_ps('Get-Process | Where-Object {$_.ProcessName.Contains("cloudia") } | Stop-Process -Force')
 #start_pro = s.run_ps("Start-Process -FilePath 'C:\Sobey\EMDC\MPC\Cloudia\cloudia.exe' -credential administrator")
 #dir_this = s.run_ps('$PSScriptRoot')
 #print(dir_this.std_out)
 #start_pro.std_err
+
+from fabric import Connection
+# host='10.78.43.10:22022'
+host='xuedant@10.78.104.8:22'
+# gatewayhost='xuedan@10.78.104.8:60022'
+command = 'ls'
+# c=Connection(host,gateway=Connection(gatewayhost),connect_kwargs={'key_filename':'/home/chry/.ssh/id_rsa'})
+c=Connection(host=host,connect_kwargs={'key_filename':'/home/chry/.ssh/id_rsa'})
+result = c.run(command=command)
+
+c.close()
+
+# 'host=host:port'
+# with Connection('host') as c:
+#     c.run('command')
+#     c.put('file')
+# env.gateway = '192.168.181.2'                             # 指定堡垒机 ip
+# env.hosts = ['192.168.181.111', '192.168.181.112']        # 指定 hosts 远程主机
+# env.key_filename = '/path/to/id_rsa'     # 指定你的私钥文件
+# env.user = 'username'                    # 指定用户名
+#
+# def touchfile():                         # 随便创建一个任务，用来测试
+#     run('touch /tmp/www.txt')
