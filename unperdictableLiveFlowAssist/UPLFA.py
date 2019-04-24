bf393d12fcdec07ebc6de147e610ac8cd652a2f4
chry-u <chrysanthemum1988@gmail.com>
2019-04-24 22:24:49 +0800

    20190424.1

--- unperdictableLiveFlowAssist/UPLFA.py
+++ unperdictableLiveFlowAssist/UPLFA.py
@@ -1,6 +1,6 @@
 from excelTrigger.readExcel import readExcel
 import os
-import  time
+import time
 import re
 import datetime
 
@@ -63,47 +63,61 @@ for item in objDict:
 1.0 0.0 1.0
     '''
     # print(objDict[item][1], time.localtime(objDict[item][2] + 1546574130.0), objDict[item][3])
-    print(re.split('[-——]',objDict[item][3]))
-    print(objDict[item][4], objDict[item][5], objDict[item][15])
-    print(objDict[item][6], objDict[item][7], objDict[item][8])
+    dateSerial = objDict[item][2]
+    dateSeconds = (dateSerial - 25569) * 86400.0
+    startDate = datetime.datetime.utcfromtimestamp(dateSeconds)
+    # time.localtime(objDict[item][2] + 1547438126.0)
+    timeRange = re.split('[-——]', objDict[item][3])
+    startTime = datetime.datetime.strptime(timeRange[0], '%H:%M')
+    endTime = datetime.datetime.strptime(timeRange[1], '%H:%M')
+    startDatetime = datetime.datetime(startDate.year, startDate.month, startDate.day, hour=startTime.hour,
+                                      minute=startTime.minute)
+    endDatetime = datetime.datetime(startDate.year, startDate.month, startDate.day, hour=endTime.hour,
+                                    minute=endTime.minute)
+
+    isLive = objDict[item][6]
+    isRecode = objDict[item][8]
+    programChannel = objDict[item][5]
+    source = objDict[item][15]
+    print('----')
+    if '\n' in source:
+        source= re.sub('[主:|备:|主：|备：|主|备]', '', source)
+        listone = re.split('\n', source)
+        print(listone[0])
+        print(listone[1])
+    else:
+        pass
+    programName = objDict[item][4]
 
     if item > 5:
         break
-    # firstsell = re.split('/', objDict[item][0])
-    # secondsell = re.split(':', objDict[item][1])
-    # thirdsell = re.split('/', objDict[item][2])
-    # fourthsell = re.split(':', objDict[item][3])
-    # startdate = datetime.datetime(int(firstsell[0]), int(firstsell[1]), int(firstsell[2]), int(secondsell[0]),
-    #                               int(secondsell[1]))
-    # enddate = datetime.datetime(int(thirdsell[0]), int(thirdsell[1]), int(thirdsell[2]), int(fourthsell[0]),
-    #                             int(fourthsell[1]))
 
 # Task.objects.get_or_create(taskName=objDict[item][4])
 # Staff.objects.get_or_create(department=objDict[item][8], staffName=objDict[item][9])
 # WorkPackage.objects.create(
-#     startDate=startdate,
-#     endDate=enddate,
-#     programName=objDict[item][7],
-#     programChannel=objDict[item][8],
-#     inPutStream=objDict[item][9],
-#     isRecode=objDict[item][10],
-#     isLive=objDict[item][11],
-#     notes=objDict[item][12],
-#     task=Task.objects.get(taskName=objDict[item][4]),
-#     adminStaff=Staff.objects.get(department=objDict[item][8], staffName=objDict[item][9]),
+    startDate=startDate,
+    endDate=endDatetime,
+    programName=programName,
+    programChannel=programChannel,
+    inPutStream=objDict[item][9],
+    isRecode=isRecode,
+    isLive=isLive,
+    notes=objDict[item][12],
+    # task=Task.objects.get(taskName=objDict[item][4]),
+    # adminStaff=Staff.objects.get(department=objDict[item][8], staffName=objDict[item][9]),
 # )
 
-print(time.localtime(43474.0 + 1547438126.0))
+# print(time.localtime(43474.0 + 1547438126.0))
 # print(time.time(2019/1/5))
 # print(time.time(datetime.date(9999, 12, 31)))
 # print(datetime.date.(datetime.date(2005, 7, 8)))
 dt = "2019-01-5 0:0:0"
 dt2 = "2019-01-5 0:0:3"
 
-#转换成时间数组
+# 转换成时间数组
 timeArray = time.strptime(dt, "%Y-%m-%d %H:%M:%S")
 timeArray2 = time.strptime(dt2, "%Y-%m-%d %H:%M:%S")
-#转换成时间戳
+# 转换成时间戳
 timestamp = time.mktime(timeArray)
 timestamp2 = time.mktime(timeArray2)
-print(timestamp-timestamp2)
+# print(timestamp - timestamp2)
