e6853118a70606b7fb9efc86a833b857d9f1664a
chry-u <chrysanthemum1988@gmail.com>
2019-05-15 20:04:38 +0800

    20190515.2

--- devices/monitorRoom/VSMplayerAss.py
+++ devices/monitorRoom/VSMplayerAss.py
@@ -1,9 +1,11 @@
 import json
 import subprocess
 
-temfile = subprocess.run("ansible-playbook VSMplayer.yml ", shell=True, stdout=subprocess.PIPE)
+temfile = subprocess.run("ansible-playbook VSMplayer.yml ", shell=True,
+                         # stdout=subprocess.PIPE
+                         )
 print('----')
-# print(temfile.stdout)
-json_data = json.loads(temfile.stdout)
-for line in json_data['plays'][0]['tasks'][1]['hosts']['ansible_connection=ssh']['stdout_lines']:
-    print(line)
+print(temfile.stdout)
+# json_data = json.loads(temfile.stdout)
+# for line in json_data['plays'][0]['tasks'][1]['hosts']['ansible_connection=ssh']['stdout_lines']:
+#     print(line)
